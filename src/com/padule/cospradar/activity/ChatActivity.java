2163fb12297a22f184d343ee93b0ebff714e7ed3
Konifar <yahpeycoy0403@gmail.com>
2014-05-25 10:45:55 +0900

    add action bar icon.

--- src/com/padule/cospradar/activity/ChatActivity.java
+++ src/com/padule/cospradar/activity/ChatActivity.java
@@ -1,9 +1,14 @@
 package com.padule.cospradar.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.padule.cospradar.MainApplication;
 import com.padule.cospradar.R;
 import com.padule.cospradar.base.BaseActivity;
 import com.padule.cospradar.data.Charactor;
@@ -28,9 +33,21 @@ public class ChatActivity extends BaseActivity {
     private void initActionBar(Charactor charactor) {
         ActionBar bar = getSupportActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayShowHomeEnabled(true);
         bar.setDisplayShowTitleEnabled(true);
         bar.setHomeButtonEnabled(true);
         bar.setTitle(charactor.getNameAndTitle());
+
+        setActionBarIcon(bar, charactor);
+    }
+
+    private void setActionBarIcon(final ActionBar bar, Charactor charactor) {
+        MainApplication.imageLoader.loadImage(charactor.getImageUrl(), new SimpleImageLoadingListener() {
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap bitmap) {
+                bar.setIcon(new BitmapDrawable(getResources(), bitmap));
+            }
+        });
     }
 
     @Override
