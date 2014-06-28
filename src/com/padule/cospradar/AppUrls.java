3ee648379884cdb1bb4a5f50084389da026e3675
Konifar <yahpeycoy0403@gmail.com>
2014-06-28 17:29:52 +0900

    apply character comment.

--- src/com/padule/cospradar/AppUrls.java
+++ src/com/padule/cospradar/AppUrls.java
@@ -6,22 +6,15 @@ public class AppUrls {
     private static final String API_URL = Constants.APP_URL;
 
     private static final String PATH_CHARACTORS = "/charactors";
-    private static final String PATH_CHARACTOR_COMMENTS = "/charactor_comments";
     private static final String PATH_USERS = "/users";
-    private static final String PATH_COMMENT_LIST = "/comment_list";
 
-    private static final String PARAM_PAGE = "page";
     public static final String PARAM_NAME = "name";
     public static final String PARAM_TITLE = "title";
     public static final String PARAM_IMAGE = "image";
     public static final String PARAM_TEXT = "text";
-    public static final String PARAM_COMMENT_CHARACTOR_ID = "comment_charactor_id";
     public static final String PARAM_USER_ID = "user_id";
 
     private static final String EXT_JSON = ".json";
-    private static final String Q = "?";
-    private static final String AND = "&";
-    private static final String EQ = "=";
 
     public static String getCharactorsCreate() {
         StringBuffer sb = new StringBuffer();
@@ -37,31 +30,6 @@ public class AppUrls {
         sb.append(PATH_USERS);
         sb.append(EXT_JSON);
         return sb.toString();
-    }
-
-    public static String getCharactorCommentsCommentList(int commentCharactorId, int page) {
-        StringBuffer sb = new StringBuffer();
-        sb.append(API_URL);
-        sb.append(PATH_CHARACTOR_COMMENTS);
-        sb.append(PATH_COMMENT_LIST);
-        sb.append(EXT_JSON);
-        sb.append(Q);
-        sb.append(createParam(PARAM_COMMENT_CHARACTOR_ID, commentCharactorId));
-        sb.append(AND);
-        sb.append(createParam(PARAM_PAGE, page));
-        return sb.toString();
-    }
-
-    private static String createParam(String key, int value) {
-        return createParam(key, value + "");
-    }
-
-    private static String createParam(String key, String value) {
-        if (value == null || "".equals(value) || "null".equals(value)) {
-            return "";
-        } else {
-            return key + EQ + value;
-        }
     }
 
 }
--- src/com/padule/cospradar/service/APIService.java
+++ src/com/padule/cospradar/service/APIService.java
@@ -24,12 +24,14 @@ public interface APIService {
     static final String PATH_CHARACTORS = "/charactors";
     static final String PATH_CHARACTOR_COMMENTS = "/charactor_comments";
     static final String PATH_CHARACTOR_LOCATIONS = "/charactor_locations";
+    static final String PATH_COMMENT_LIST = "/comment_list";
 
     static final String PARAM_PAGE = "page";
     static final String PARAM_TITLE = "title";
     static final String PARAM_LIMIT = "limit";
     static final String PARAM_DESC = "desc";
     static final String PARAM_CHARACTOR_ID = "charactor_id";
+    static final String PARAM_COMMENT_CHARACTOR_ID = "comment_charactor_id";
     static final String PARAM_USER_ID = "user_id";
     static final String PARAM_LATITUDE = "latitude";
     static final String PARAM_LONGITUDE = "longitude";
@@ -43,6 +45,11 @@ public interface APIService {
     @POST(PATH_CHARACTOR_COMMENTS + EXT_JSON)
     void postCharactorComments(@Body CharactorComment comment, 
             Callback<CharactorComment> cb);
+
+    @GET(PATH_CHARACTOR_COMMENTS + PATH_COMMENT_LIST + EXT_JSON)
+    void getCharactorCommentsCommentList(@Query(PARAM_COMMENT_CHARACTOR_ID) int commentCharactorId,
+            @Query(PARAM_PAGE) int page,
+            Callback<List<Charactor>> cb);
 
     @GET(PATH_CHARACTORS + EXT_JSON)
     void getCharactors(@QueryMap Map<String, String> params, 
